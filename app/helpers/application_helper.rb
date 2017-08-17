ea4f989f825ea31fdea6ce450a2468b7411f1a94
trolandus <jan.belczacki@o2.pl>
2017-08-17 16:02:27 +0000

    Add title functionality for show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    #splat operator (*) means that any argument passed from this point will be
+    #available inside the method as an array
+    def title(*parts)
+        unless parts.empty?
+            content_for :title do
+                (parts << "Ticketee").join(" - ")
+            end
+        end
+    end
 end
