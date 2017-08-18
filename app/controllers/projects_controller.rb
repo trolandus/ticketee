5e99d01ca4bb0431f2d4d7c4a9aab69ed8e138b7
trolandus <jan.belczacki@o2.pl>
2017-08-18 10:21:07 +0000

    User can update a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+    before_action :find_project, only: [:show, :edit, :update]
+    
     def index
         @projects = Project.all
     end
@@ -21,11 +23,27 @@ class ProjectsController < ApplicationController
     end
     
     def show
-       @project = Project.find(params[:id]) 
+    end
+    
+    def edit
+    end
+    
+    def update
+        if @project.update(project_params)
+            flash[:notice] = "Project has been updated"
+            redirect_to @project
+        else
+            flash[:alert] = "Project has not been updated"
+            render "edit"
+        end
     end
     
     private
     def project_params
        params.require(:project).permit(:name, :description) 
+    end
+    
+    def find_project
+        @project = Project.find(params[:id])
     end
 end
